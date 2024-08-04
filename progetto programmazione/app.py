a9872d48455982de78bf807b69a3feb31d0dcb03
Anomis1999 <simycon99@hotmail.it>
2024-08-04 13:25:22 +0200

    Update app.py

--- progetto programmazione/app.py
+++ progetto programmazione/app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from flask import Flask, render_template, request
 from calcolo_calorico import calcolo_bmr, calcola_tdee, ripartizione_calorica, crea_grafico_ripartizione, crea_grafico_ripartizione_barre
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
@@ -37,31 +38,47 @@ def calcola():
 
         # Creazione delle ricette per ogni giorno della settimana
         menu_settimanale = {giorno: {
-            'Colazione': pd.DataFrame(filtra_ricette('Colazione', colazione)),
-            'Pranzo': pd.DataFrame(filtra_ricette('Pranzo', pranzo)),
-            'Cena': pd.DataFrame(filtra_ricette('Cena', cena)),
-            'Spuntino': pd.DataFrame(filtra_ricette('Spuntino', spuntino))
+            'Colazione': filtra_ricette('Colazione', colazione),
+            'Pranzo': filtra_ricette('Pranzo', pranzo),
+            'Cena': filtra_ricette('Cena', cena),
+            'Spuntino': filtra_ricette('Spuntino', spuntino)
         } for giorno in giorni_settimana}
 
-        def media_kcal(settimana):
-            media_calorie = {}
-            for giorno, pasti in settimana.items():
-                media_calorie[giorno] = {}
-                for pasto, ricette in pasti.items():
-                    if not ricette.empty:
-                        media_calorie[giorno][pasto] = ricette['Calorie'].mean()
-                    else:
-                        media_calorie[giorno][pasto] = None
-            return media_calorie
+        # Calcolo delle medie delle calorie
+        media_kcal = {}
+        for giorno, pasti in menu_settimanale.items():
+            media_kcal[giorno] = {}
+            for pasto, ricette in pasti.items():
+                if ricette:
+                    media_kcal[giorno][pasto] = sum([ricetta['Calorie'] for ricetta in ricette]) / len(ricette)
+                else:
+                    media_kcal[giorno][pasto] = None
 
-        media_calorie = media_kcal(menu_settimanale)
+        # Creazione dei grafici
+        giorni = list(media_kcal.keys())
+        dati = [colazione, pranzo, cena, spuntino]
+        pasti = ["Colazione", "Pranzo", "Cena", "Spuntino"]
 
-        # Renderizza il template con i risultati
+        for giorno in giorni:
+            pasti_giornalieri = list(media_kcal[giorno].keys())
+            medie_pasti = []
+            for pasto in pasti_giornalieri:
+                if media_kcal[giorno][pasto] is not None:
+                    medie_pasti.append(media_kcal[giorno][pasto])
+
+            fig, ax = plt.subplots()
+            ax.plot(pasti_giornalieri, medie_pasti, label="Calorie reali")
+            ax.plot(pasti, dati, label="Calorie attese")
+            ax.legend()
+            ax.set_title(f"Bilancio calorie giornaliere - {giorno}")
+            ax.set_xlabel("Pasti del giorno")
+            ax.set_ylabel("Calorie calcolate")
+            plt.savefig(f"static/grafico_{giorno}.png")
+
         return render_template('risultato.html', 
                                bmr=bmr, tdee=tdee,
                                colazione=colazione, pranzo=pranzo, cena=cena, spuntino=spuntino,
-                               menu_settimanale=menu_settimanale,
-                               media_kcal=media_kcal)
+                               menu_settimanale=menu_settimanale)
     
     except ValueError as e:
         error_message = str(e)
